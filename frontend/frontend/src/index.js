042b3da0af68f0a2aae5ae8fc937d8aaea939614
Atul Chandran <atulchandran05@gmail.com>
2021-02-08 11:35:51 +0530

    Corrected route

--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -7,19 +7,12 @@ import { Route, BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render((
-  <Router>
-     <Route path = "/" component = {App} />
-      <Route path = "/about" component = {EmployerLogin} />
-  </Router>
-), document.getElementById('root'))
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
